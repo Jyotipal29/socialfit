1f9dd59bfaf772c891a28ebe2abe90cc5be0c6a8
jyoti pal <paljyoti0129@gmail.com>
2023-09-11 17:59:18 +0530

    style updated

--- src/component/VideoCard.jsx
+++ src/component/VideoCard.jsx
@@ -1,18 +1,19 @@
 function VideoCard({ video, tags, heading }) {
   return (
-    <div className="flex flex-col shadow-md hover:shadow-lg items-center py-5 space-y-4 h-[300px] rounded-md group relative">
+    <div className=" rounded-lg flex flex-col justify-center items-center space-y-2 py-4 shadow-md ">
+      <h1 className="text-xl uppercase font-bold">{heading}</h1>
+
       <video
         src={video}
         width={350}
-        controls={false}
-        className="absolute w-full h-full bg-black pointer-events-none z-0"
+        controls
+        className=" w-full h-full bg-black  "
       />
-      <div className="trnsition-all duration-200 bg-black/50 p-5 absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-full h-full flex flex-col items-center justify-between text-white ">
-        <h1 className="text-xl uppercase font-bold">{heading}</h1>
+      <div>
         <div className="p-2"></div>
         <div className="flex gap-4">
           {tags.map((tag) => (
-            <span className="border-2 py-1 px-5 border-white  rounded-md text-[14px]">
+            <span className="border-2 border-gray-600 py-1 px-5   rounded-md text-[14px]">
               {tag}
             </span>
           ))}
